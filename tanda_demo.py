99dac5d174f8e131493a6729c489effe0ae563c7
ZararAslam <zararawan485@gmail.com>
2025-05-18 16:25:01 +0100

    Update tanda_demo.py

--- tanda_demo.py
+++ tanda_demo.py
@@ -41,7 +41,7 @@ st.markdown("""
 
 # Display centered company logo
 st.markdown("<div style='text-align: center; padding-bottom: 10px;'>", unsafe_allow_html=True)
-st.image("meraki-logo.png", width=180)
+st.image("LOGO-TRANSPARENT.png", width=180)
 st.markdown("</div>", unsafe_allow_html=True)
 
 # Initialize session state for threads and messages
